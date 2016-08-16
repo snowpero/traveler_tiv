ed8a40bddecb23e92df07ec32cbcc378f43f7a48
Gil-Yong Park <gypark@braincommerce.com>
2016-08-16 10:57:43 +0900

    init branch

--- src/com/ninis/tiv/Defines.java
+++
@@ -1,8 +0,0 @@
-package com.ninis.tiv;
-
-public class Defines {
-	public static final String URL_MAIN = "http://tiv85.blog.me/";
-	
-	public static final String PARSE_APP_ID = "XOuLLLjQedi12OJgcpoBBmJIQkrQoSNdtJ81CkyO";
-	public static final String PARSE_CLIENT_KEY = "PZz5tkHQh7hklynIA25hBWNEwx1rdA6eIOfAobRe";
-}
